9f94113438115d78221b285791a68cb2130db6e8
biz777 <bizgreen777@gmail.com>
2026-06-26 23:23:31 +0100

    Wrap children in a main tag within the body

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,9 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
